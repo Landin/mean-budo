2396f9419c099cc981c231f0c396d71f65234a23
Landin <landinius84@gmail.com>
2014-10-13 09:15:58 +0200

    pagination bas + trigger skrivare

--- public/app/loggswipe/mvLoggswipeCtrl.js
+++ public/app/loggswipe/mvLoggswipeCtrl.js
@@ -1,4 +1,4 @@
-define(['xml2json'], function () { 
+define(['xml2json', 'uiBootstrap'], function () { 
 
   function mvLoggswipeCtrl($location, mvLoggswipe){
     'use strict';
@@ -11,6 +11,7 @@ define(['xml2json'], function () {
       });
     };
     
+    // Funktion för att ladda ner Loggswipes till hårdisk. 
      vm.getloggswipe = function () {  
       //Create x2js instance with default config
       var x2js = new X2JS();
@@ -19,15 +20,38 @@ define(['xml2json'], function () {
       var filename = "nisse.xml";
       download(filename, xmlDocStr);
     }
-     
+   
     function download(filename, text) {
       var pom = document.createElement('a');
       var octet = 'data:text/plain;charset=utf-8,';
       pom.setAttribute('href', octet + encodeURIComponent(text));
       pom.setAttribute('download', filename);
       pom.click();
-    } 
+    }
+    
+    // Pagination för att dela upp LoggSwipe till flera sidor.
+    vm.loggpage = function () {
+      vm.totalItems = 10;
+      vm.currentPage = 4;
+
+      vm.setPage = function (pageNo) {
+        vm.currentPage = pageNo;
+      };
+
+      vm.pageChanged = function() {
+        console.log('Page changed to: ' + vm.currentPage);
+      };
+
+      vm.maxSize = 5;
+      vm.bigTotalItems = 175;
+      vm.bigCurrentPage = 1;
+    }
+    
+    // Funktion för att trigga Skrivaren för att skriva ut DOM
   }
+      $state.get().forEach(function (state) {
+        console.log(state.name);
+          })
 
   return mvLoggswipeCtrl;
 });
